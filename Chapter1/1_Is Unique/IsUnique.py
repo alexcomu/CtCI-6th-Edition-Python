f8fc279ec74dc69f19c5ae48d6b167a6f9606545
alexcomu <alex.comunian@gmail.com>
2017-05-01 18:10:39 +0200

    my unique version

--- Chapter1/1_Is Unique/IsUnique.py
+++ Chapter1/1_Is Unique/IsUnique.py
@@ -3,18 +3,9 @@ import unittest
 
 
 def unique(string):
-    # Assuming character set is ASCII (128 characters)
-    if len(string) > 128:
-        return False
-
-    char_set = [False for _ in range(128)]
-    for char in string:
-        val = ord(char)
-        if char_set[val]:
-            # Char already found in string
+    for letter in string:
+        if string.count(letter) > 1:
             return False
-        char_set[val] = True
-
     return True
 
 
